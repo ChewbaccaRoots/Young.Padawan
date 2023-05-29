b26f64645c657d6bbd31846c903483e74bf86114
Rafael Prosdoskimis <130113176+ChewbaccaRoots@users.noreply.github.com>
2023-05-29 20:04:29 -0300

    Update Ordenador.py

--- Ordenador.py
+++ Ordenador.py
@@ -15,10 +15,24 @@ class ordenador:
             # Para isso, troca de lugar os elementos nas podições i e posicao_do_minimo
             lista[i], lista[posicao_do_minimo] = lista[posicao_do_minimo], lista[i]
 
+
     def bolha(self, lista):
+            fim = len(lista)
+
+            for i in range(fim-1, 0, -1):
+                for j in range(i):
+                    if lista[j] > lista[j+1]:
+                        lista[j], lista[j+1] = lista[j+1], lista[j]
+
+
+    def bolha_curta(self, lista):
         fim = len(lista)
 
         for i in range(fim-1, 0, -1):
+            trocou = False
             for j in range(i):
                 if lista[j] > lista[j+1]:
                     lista[j], lista[j+1] = lista[j+1], lista[j]
+                    trocou = True
+            if not trocou:
+                return
