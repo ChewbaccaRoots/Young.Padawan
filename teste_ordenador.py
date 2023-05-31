218995efd7dbb6520d35a0821a2b64106b642a96
Rafael Prosdoskimis <130113176+ChewbaccaRoots@users.noreply.github.com>
2023-05-30 21:39:51 -0300

    teste_ordenador.py

---
+++ teste_ordenador.py
@@ -0,0 +1,44 @@
+# Testes automatizados, para os ordenadores com dois algoritmos e situações diferentes
+
+import ordenador
+import pytest
+import conta_tempos
+
+class TestaOrdenador:
+
+    @pytest.fixture
+    def o(self):
+        return ordenador.ordenador()
+
+    @pytest.fixture
+    def l_quase(self):
+        c = conta_tempos.ContaTempos()
+        return c.lista_quase_ordenada(100)
+
+    @pytest.fixture
+    def l_aleat(self):
+        c = conta_tempos.ContaTempos()
+        return c.lista_aleatoria(100)
+
+    def esta_ordenada(self, l):
+        for i in range(len(l)-1):
+            if l[i] > l[i+1]:
+                return False
+        return True
+
+
+    def testa_bolha_curta_aleat(self, o, l_aleat):
+        o.bolha_curta(l_aleat)
+        assert self.esta_ordenada(l_aleat)
+
+    def testa_selacao_direta_aleat(self, o, l_aleat):
+        o.selecao_direta(l_aleat)
+        assert self.esta_ordenada(l_aleat)
+
+    def testa_bolha_curta_quase(self, o, l_quase):
+        o.bolha_curta(l_quase)
+        assert self.esta_ordenada(l_quase)
+
+    def testa_selacao_direta_quase(self, o, l_quase):
+        o.selecao_direta(l_quase)
+        assert self.esta_ordenada(l_quase)
